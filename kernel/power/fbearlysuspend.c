f1b558a7f6c78d5467d986317a206e66c1fb2a06
Rox-Ace2 <rooobertinhooo@gmail.com>
2013-10-20 14:21:11 +0200

    Update fbearlysuspend.c

--- kernel/power/fbearlysuspend.c
+++ kernel/power/fbearlysuspend.c
@@ -28,14 +28,6 @@ unsigned int fbdelay_ms = 350;
 module_param(fbdelay, bool, 0644);
 module_param(fbdelay_ms, uint, 0644);
 
-/* Electron beam animation fix (CRT-ON EFFECT) taken from cocafe */
-
-bool fbdelay_first = false;
-unsigned int fbdelay_first_ms = 350;
-
-module_param(fbdelay_first, bool, 0644);
-module_param(fbdelay_first_ms, uint, 0644);
-
 #include "power.h"
 
 static wait_queue_head_t fb_state_wq;
@@ -74,11 +66,6 @@ static void stop_drawing_early_suspend(struct early_suspend *h)
 static void start_drawing_late_resume(struct early_suspend *h)
 {
 	unsigned long irq_flags;
-	
-	/* Electron beam animation fix (CRT-ON EFFECT) taken from cocafe */
-	if (fbdelay_first) {
-	msleep(fbdelay_first_ms);
-	}
 
 	spin_lock_irqsave(&fb_state_lock, irq_flags);
 	fb_state = FB_STATE_DRAWING_OK;
